5ec6841f531d8f45cbaf36f9b632ec3d96a49fc0
brice <brice@libcast.com>
2019-05-31 16:40:45 +0200

    Remove unused namespace

--- src/Application/Cli/UpdateCommand.php
+++ src/Application/Cli/UpdateCommand.php
@@ -6,7 +6,6 @@ use CompoLab\Application\GitlabRepositoryManager;
 use Gitlab\Client as Gitlab;
 use Gitlab\Model\Project;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
